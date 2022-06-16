ffe7cb82b9df28437977b33e26d6d76c2f8f6ab9
Srdjan <srdjan.zupljanin@jsguru.io>
2022-06-16 09:43:49 +0200

    add assets to the package

--- src/database/migrator/migrator.ts
+++ src/database/migrator/migrator.ts
@@ -31,7 +31,7 @@ export class Migrator implements IMigrator {
           [
             filepath,
             fs
-              .readFileSync(path.join(__dirname, 'sample-migration.tmp'))
+              .readFileSync(path.join(__dirname, 'sample-migration.template'))
               .toString(),
           ],
         ],
